835afcdfb753f055f4ca2313814bc7a7be01ca49
kim <jaajapie@hotmail.com>
2022-03-17 21:22:27 +0700

    init cover specific page

--- pages/buycover.js
+++ pages/buycover.js
@@ -1,13 +1,13 @@
 import React from "react";
-import BuyCover from "../src/views/BuyCover";
+import Coverage from "../src/views/BuyCover";
 import MainLayout from "../src/layouts/MainLayout";
 import Header from "../src/components/Header";
 
 const buycover = () => {
   return (
     <MainLayout>
-      <Header title={`CONVEST FINANCE: BUYCOVER`}></Header>
-      <BuyCover></BuyCover>
+      <Header title={`CONVEST FINANCE: Buycover`}></Header>
+      <Coverage></Coverage>
     </MainLayout>
   );
 };
